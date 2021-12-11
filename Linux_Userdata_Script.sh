bd88bcb8cf2093f369c02cdb974e89e023c2b2a3
Jordan Chen <ceeeffque@gmail.com>
2021-12-11 10:20:21 +0800

    Create Linux_Userdata_Script.sh

---
+++ Linux_Userdata_Script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+sudo su
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt-get install ca-certificates curl gnupg lsb-release -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+cd /opt
+sudo git clone --recurse-submodules https://github.com/cobbr/Covenant
+cd Covenant/Covenant
+sudo docker build -t covenant .
+sudo docker run -t -p 7443:7443 -p 8080:8080 -p 4433:4433 --name covenant -v /opt/Covenant/Covenant/Data:/app/Data covenant --username AdminUser --computername 0.0.0.0
+sudo apt install expect -y
+echo -e '#!/bin/expect\nspawn sudo docker run -it -p 7443:7443 -p 8080:8080 -p 4433:4433 --name covenant -v /opt/Covenant/Covenant/Data:/app/Data covenant --username AdminUser --computername 0.0.0.0\nexpect "Password:"' > script
+echo 'send "\n"' >> script
+echo 'expect *root*' >> script
+echo 'sleep infinity' >> script
+echo 'send "\n"' >> script
+echo "expect *root*" >> script
+echo 'sleep infinity' >> script
+chmod 777 script
+./script
